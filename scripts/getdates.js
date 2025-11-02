3c8db3e61e2e3561fcdda621b5d8d92cad74961e
Poko1706 <pokoarenas@gmail.com>
2025-11-01 18:56:53 -0600

    ok

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -39,3 +39,5 @@ year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}<
 month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
 day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
 dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
